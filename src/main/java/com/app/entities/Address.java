1a60273773242f25e8b6c5fd2e2f193fbd045335
JoseDev <joseslopez08k@gmail.com>
2025-06-23 12:01:14 +0200

    Added Address entity

---
+++ src/main/java/com/app/entities/Address.java
@@ -0,0 +1,17 @@
+package com.app.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
