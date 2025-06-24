38b212ae9f9207259990a3cabb0512024b2b1f83
JoseDev <joseslopez08k@gmail.com>
2025-06-24 18:19:57 +0200

    Exposed IDs for every class with testing purposes

--- src/main/java/com/app/config/RestRepositoryConfig.java
+++ src/main/java/com/app/config/RestRepositoryConfig.java
@@ -1,5 +1,8 @@
 package com.app.config;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.Type;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -7,8 +10,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class RestRepositoryConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        // Expose IDs for all entities automatically
+        config.exposeIdsFor(
+                entityManager.getMetamodel().getEntities().stream()
+                        .map(Type::getJavaType)
+                        .toArray(Class[]::new)
+        );
         cors.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
