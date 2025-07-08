a026aade834378d3e83469b7c49811ea6ea3a2be
JoseDev <joseslopez08k@gmail.com>
2025-07-08 19:21:25 +0200

    Add validation annotations to DiscountCode entity
    Applied @NotNull, @Size, and @Min constraints to code, discountAmount, and expiryDate fields
    
    Ensured data integrity for discount code management

--- src/main/java/com/app/entities/DiscountCode.java
+++ src/main/java/com/app/entities/DiscountCode.java
@@ -3,6 +3,9 @@ package com.app.entities;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -25,10 +28,16 @@ public class DiscountCode {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Code is required")
+    @Size(min = 3, max = 30, message = "Code must be between 3 and 30 characters")
     private String code;
+
+    @NotNull(message = "Discount percentage is required")
+    @Min(value = 0, message = "Discount percentage must be at least 0")
     private Integer discountAmount;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @NotNull(message = "Expiry date is required")
     private LocalDate expiryDate;
 
     private boolean isActive;
