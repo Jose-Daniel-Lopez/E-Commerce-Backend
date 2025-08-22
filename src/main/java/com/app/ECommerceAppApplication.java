7052d4e65e7e008cca1fb40a01c6eef0be069c02
JoseDev <joseslopez08k@gmail.com>
2025-08-22 18:41:43 +0200

    feat: Add dotenv dependency and load environment variables for configuration

--- src/main/java/com/app/ECommerceAppApplication.java
+++ src/main/java/com/app/ECommerceAppApplication.java
@@ -1,5 +1,6 @@
 package com.app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ECommerceAppApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv.configure().ignoreIfMissing().load();
 		SpringApplication.run(ECommerceAppApplication.class, args);
 	}
 
